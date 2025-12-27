1aeb860dbb7e23244c01dfb50c3fb9072f59e085
alex karpovich <aakarpovich@mtt.ru>
2025-12-27 19:08:48 +0300

    feat: learn laravel project

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Post;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $title = $this->faker->realText(rand(10, 40));
+        $short_title = mb_strlen($title) > 30 ? mb_substr($title, 0, 30) . '...' : $title;
+        $created = $this->faker->dateTimeBetween('-30 days', '-1 days');
+        return [
+            'title' => $title,
+            'short_title' => $short_title,
+            'author_id' => rand(1,4),
+            'descr' => $this->faker->realText(rand(100, 500)),
+            'created_at' => $created,
+            'updated_at' => $created
+        ];
+    }
+}
